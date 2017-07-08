58e64768b3145df7d5b64149752a6ab41bfebcec
Brian Wilbur <brian@bwilbur.com>
2017-07-08 19:06:24 -0400

    Added button output

--- run.py
+++ run.py
@@ -1,18 +1,41 @@
+#
 # Controller view by Brian Wilbur
+# https://github.com/oblivion-hymns
 #
 import pygame;
 
-pygame.joystick.init();
+def getJoystick():
+    """Just get first connected joystick for now"""
 
-joystick = None;
-joystickIndex = -1;
-joystickCount = pygame.joystick.get_count();
-for x in range(joystickCount):
-    if pygame.joystick.Joystick(x):
-        joystick = pygame.joystick.Joystick(x);
-        joystickIndex = x;
+    joystick = None;
+    joystickIndex = -1;
+    joystickCount = pygame.joystick.get_count();
+    for x in range(joystickCount):
+        potentialJoystick = pygame.joystick.Joystick(x);
+        if potentialJoystick:
+            return potentialJoystick;
 
-if joystickIndex > -1:
-    print('Controller found in slot ' + joystickIndex);
-else:
-    print('No controllers found. Please ensure your controller is plugged in and turned on.');
+    return None;
+
+def readInput(joystick):
+    numButtons = joystick.get_numbuttons();
+
+    for i in range(numButtons):
+        console.log('Button i: ' + str(i));
+
+    return;
+
+def main():
+    pygame.joystick.init();
+
+    joystick = getJoystick();
+    runLoop = True;
+
+    if (joystick):
+        print('Controller found. Listening for input...');
+        while (runLoop):
+            readInput(joystick);
+    else:
+        print('No controllers found. Please ensure your controller is plugged in and turned on.');
+
+main();
