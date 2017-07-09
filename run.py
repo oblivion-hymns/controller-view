ea5698817fe4fe2274c5b68151f00419d774b7bd
Brian Wilbur <brian@bwilbur.com>
2017-07-08 20:54:59 -0400

    sigh

--- run.py
+++ run.py
@@ -47,7 +47,7 @@ def main():
 
         while runLoop:
             for event in pygame.event.get():
-                console.log('event found');
+                print('event found');
                 if event.type == pygame.JOYBUTTONDOWN:
                     print("Joystick button pressed.")
                 if event.type == pygame.JOYBUTTONUP:
