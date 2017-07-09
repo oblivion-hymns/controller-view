6f5076e876ee7415899d9d59572973f6c094cc13
Brian Wilbur <brian@bwilbur.com>
2017-07-09 00:39:37 -0400

    Prettified

--- run.py
+++ run.py
@@ -11,61 +11,81 @@ class Buttons:
         'index': 0,
         'image': pygame.image.load("./img/buttons/a.png"),
         'isPressed': False,
-        'position': [0, 0]
+        'position': [368, 128],
+        'size': [48, 48],
+        'rotation': 0
     };
     B = {
         'index': 1,
         'image': pygame.image.load("./img/buttons/b.png"),
         'isPressed': False,
-        'position': [32, 0]
+        'position': [400, 96],
+        'size': [48, 48],
+        'rotation': 0
     };
     X = {
         'index': 2,
         'image': pygame.image.load("./img/buttons/x.png"),
         'isPressed': False,
-        'position': [64, 0]
+        'position': [336, 96],
+        'size': [48, 48],
+        'rotation': 0
     };
     Y = {
         'index': 3,
         'image': pygame.image.load("./img/buttons/y.png"),
         'isPressed': False,
-        'position': [96, 0]
+        'position': [368, 66],
+        'size': [48, 48],
+        'rotation': 0
     };
     LEFT_BUMPER = {
         'index': 4,
         'image': pygame.image.load("./img/buttons/lb.png"),
         'isPressed': False,
-        'position': [128, 0]
+        'position': [64, -38],
+        'size': [112, 112],
+        'rotation': 10
     };
     RIGHT_BUMPER = {
         'index': 5,
         'image': pygame.image.load("./img/buttons/rb.png"),
         'isPressed': False,
-        'position': [0, 32]
+        'position': [318, -38],
+        'size': [112, 112],
+        'rotation': -10
     };
     BACK = {
         'index': 6,
         'image': pygame.image.load("./img/buttons/back.png"),
         'isPressed': False,
-        'position': [32, 32]
+        'position': [200, 102],
+        'size': [32, 32],
+        'rotation': 0
     };
     START = {
         'index': 7,
         'image': pygame.image.load("./img/buttons/start.png"),
         'isPressed': False,
-        'position': [64, 32]
+        'position': [280, 102],
+        'size': [32, 32],
+        'rotation': 0
     };
     LEFT_STICK = {
         'index': 8,
         'image': pygame.image.load("./img/buttons/ls.png"),
         'isPressed': False,
-        'position': [96, 32]
+        'position': [88, 80],
+        'size': [80, 80],
+        'rotation': 0
     };
     RIGHT_STICK = {
         'index': 9,
         'image': pygame.image.load("./img/buttons/rs.png"),
         'isPressed': False,
-        'position': [128, 32]
+        'position': [280, 152],
+        'size': [80, 80],
+        'rotation': 0
     };
 
 def resolveButtonIndex(i):
@@ -93,27 +113,43 @@ def resolveButtonIndex(i):
 
     return 'Unknown Button';
 
-def draw(screen):
+def draw(screen, bgImage):
     screen.fill((40, 40, 40));
 
     buttonKeys = dir(Buttons);
+
+    screen.blit(bgImage, [0, 0]);
+
 
     for key in buttonKeys:
         if not key.startswith('__'):
             buttonConst = getattr(Buttons, key);
             if buttonConst and buttonConst['isPressed'] == True:
-                screen.blit(pygame.transform.scale(buttonConst['image'], (32, 32)), buttonConst['position']);
 
-    #screen.blit(pygame.transform.scale(Buttons.A['image'], (32, 32)), Buttons.A['position']);
-    #screen.blit(pygame.transform.scale(Buttons.B['image'], (32, 32)), Buttons.B['position']);
-    #screen.blit(pygame.transform.scale(Buttons.X['image'], (32, 32)), Buttons.X['position']);
-    #screen.blit(pygame.transform.scale(Buttons.Y['image'], (32, 32)), Buttons.Y['position']);
-    #screen.blit(pygame.transform.scale(Buttons.LEFT_BUMPER['image'], (32, 32)), Buttons.LEFT_BUMPER['position']);
-    #screen.blit(pygame.transform.scale(Buttons.RIGHT_BUMPER['image'], (32, 32)), Buttons.RIGHT_BUMPER['position']);
-    #screen.blit(pygame.transform.scale(Buttons.BACK['image'], (32, 32)), Buttons.BACK['position']);
-    #screen.blit(pygame.transform.scale(Buttons.START['image'], (32, 32)), Buttons.START['position']);
-    #screen.blit(pygame.transform.scale(Buttons.LEFT_STICK['image'], (32, 32)), Buttons.LEFT_STICK['position']);
-    #screen.blit(pygame.transform.scale(Buttons.RIGHT_STICK['image'], (32, 32)), Buttons.RIGHT_STICK['position']);
+                buttonPosition = buttonConst['position'];
+                buttonImage = buttonConst['image'];
+                buttonSize = buttonConst['size'];
+                buttonRotation = buttonConst['rotation'];
+
+                transformedButton = pygame.transform.scale(buttonImage, buttonSize);
+                transformedButton = pygame.transform.rotate(transformedButton, buttonRotation);
+
+                screen.blit(transformedButton, buttonPosition);
+
+    #for key in buttonKeys:
+    #    if not key.startswith('__'):
+    #        buttonConst = getattr(Buttons, key);
+    #        if buttonConst:
+#
+#                buttonPosition = buttonConst['position'];
+#                buttonImage = buttonConst['image'];
+#                buttonScale = buttonConst['size'];
+#                buttonRotation = buttonConst['rotation'];
+#
+#                transformedButton = pygame.transform.scale(buttonImage, buttonScale);
+#                transformedButton = pygame.transform.rotate(transformedButton, buttonRotation);
+#
+#                screen.blit(transformedButton, buttonPosition);
 
     pygame.display.flip();
     return;
@@ -121,7 +157,7 @@ def draw(screen):
 def main():
     pygame.init();
     pygame.joystick.init();
-    screenSize = [200, 200];
+    screenSize = [512, 342];
     screen = pygame.display.set_mode(screenSize);
     pygame.display.set_caption('Controller View');
 
@@ -135,6 +171,7 @@ def main():
             break;
 
     runLoop = True;
+    bgImage = pygame.image.load("./img/controller.png");
 
     if (joystick):
         print('Controller found. Listening for input...');
@@ -168,11 +205,11 @@ def main():
                                 buttonConst['isPressed'] = False;
                                 print(buttonName + ' released');
 
-                draw(screen);
+                draw(screen. bgImage);
     else:
         print('No controllers found. Please ensure your controller is plugged in and turned on.');
 
         while True:
-            draw(screen);
+            draw(screen, bgImage);
 
 main();
